514e5f135c66fe819da8e4b44d2417597e0f4c8e
Ahmad Ul Hoq Nadim <ahmadulhoqnadim@gmail.com>
2019-04-20 00:47:46 +0600

    setup relationship between Product and Review Model

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+    	return $this->hasMany(Review::class);
+    }
 }
