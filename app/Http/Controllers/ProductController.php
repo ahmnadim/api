cd341b198c942b5dfc05dc38e80da6f117ac204e
Ahmad Ul Hoq Nadim <ahmadulhoqnadim@gmail.com>
2019-04-27 16:16:33 +0600

    fix the problem of description.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -89,6 +89,8 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+        $request['details'] = $request->description;
+        unset($request->description);
         $product->update($request->all());
 
         return response([
