ae280e41df006fe6be3e1b65f7e0fd1b9bf47971
Ahmad Ul Hoq Nadim <ahmadulhoqnadim@gmail.com>
2019-04-28 00:35:30 +0600

    setup incorrect route exception.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -52,6 +53,12 @@ class Handler extends ExceptionHandler
             if ($exception instanceof ModelNotFoundException) {
                 return response()->json([
                     "errors"=> "Product Model Not Found."
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    "errors"=> "Incorrect Route."
                 ], Response::HTTP_NOT_FOUND);
             }
         }
