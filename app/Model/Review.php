bc051d99ea1466ce5faf65482d1b2c8c20f6d6a3
Ahmad Ul Hoq Nadim <ahmadulhoqnadim@gmail.com>
2019-04-28 17:12:05 +0600

    Review created for individual product.

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+
+	protected $fillable = ['review', 'star', 'customer'];
+
     public function product(){
     	return $this->belongsTo(Product::class);
     }
